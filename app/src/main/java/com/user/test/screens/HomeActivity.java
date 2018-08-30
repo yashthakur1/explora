36e75cf073d4b107aaca819ad236e69c7581a012
Yash Thakur <yash.coder@gmail.com>
2018-08-31 00:03:30 +0530

    animation added

--- app/src/main/java/com/user/test/screens/HomeActivity.java
+++ app/src/main/java/com/user/test/screens/HomeActivity.java
@@ -7,17 +7,18 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import com.heady.explora.R;
+import com.orhanobut.logger.Logger;
 import com.user.test.base.BaseActivity;
 import com.user.test.base.ExploraApp;
 import com.user.test.screens.adapters.UserAdapter;
 import com.user.test.screens.userModels.ResponseResult;
-import com.orhanobut.logger.Logger;
 
 import java.util.ArrayList;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import jp.wasabeef.recyclerview.animators.SlideInRightAnimator;
 
 public class HomeActivity extends BaseActivity implements HomeContract.View {
 
@@ -48,7 +49,9 @@ public class HomeActivity extends BaseActivity implements HomeContract.View {
     public void showUsers(ArrayList<ResponseResult> users) {
         LinearLayoutManager linearLayoutManagerVertical = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         final UserAdapter exploreAdapter = new UserAdapter(context, users);
+        rvExplore.setItemAnimator(new SlideInRightAnimator());
         rvExplore.setLayoutManager(linearLayoutManagerVertical);
+
         rvExplore.setAdapter(exploreAdapter);
         exploreAdapter.listener = new UserAdapter.OnItemClickListener() {
             @Override
